7afdc9c63795028261276b767504e2eb49ee7e0b
jco657 <jco657@126.com>
2017-10-12 23:21:51 +0800

    修改随机数产生算法2

--- java/DataStructureFouEdition/src/DataStructureChapter1/Array1.java
+++ java/DataStructureFouEdition/src/DataStructureChapter1/Array1.java
@@ -1,17 +1,33 @@
 package DataStructureChapter1;
 
+import java.util.HashSet;
+
 public class Array1 {
 	
 	public static void main(String[] args){
 		int n=10,size=100;
-		Integer[] values = Array1.randomDiffInteger1(n, size);
+		HashSet<Integer> set = new HashSet<Integer>();
+		
+		Integer[] values = Array1.randomDiffInteger1(n,size);
+		
 		System.out.print(n+"个元素0-"+size+"之间的随机数集合：");
 		Array1.print(values);
+		
+		System.out.println();
+		System.out.print(n+"个元素0-"+size+"之间的随机数集合：");
+		Array1.randomDiffInteger2(n,size,set);
+		Array1.print(set);
 	}
-	
 	//输出随机数
 	public static void print(Object[] value){
 		for(Object obj:value){
+			System.out.print(obj==null?"null":" "+obj.toString());
+		}
+		System.out.println();
+	}
+	//输出set版
+	public static void print(HashSet<Integer> set){
+		for(Object obj:set){
 			System.out.print(obj==null?"null":" "+obj.toString());
 		}
 		System.out.println();
@@ -49,17 +65,17 @@ public class Array1 {
 		}
 		return values;
 	}
-	//产生不同的随机数，case2：利用asList()方法
-	public static Integer[] randomDiffInteger2(int n,int size){
+	//产生不同的随机数，case2：利用HashSet方法
+	public static void randomDiffInteger2(int n,int size,HashSet<Integer> set){
 		Integer[] values = new Integer[n];
 		for(int i=0;i<values.length;i++){
-			if(0==i){
-				values[i] = new Integer((int)(Math.random()*size));
-			}else{
-				Integer tempValues = new Integer((int)(Math.random()*size));
-				if 
-			}
+			int tempValue = (int)(Math.random()*size);
+			set.add(tempValue);
 		}
-		return values;
+		int setSize = set.size();
+		
+		if(setSize<n){
+			randomDiffInteger2(n-setSize,size,set);
+		}
 	}
 }
